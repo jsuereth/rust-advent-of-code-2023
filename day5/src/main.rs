6075c24ca60acc53d8b4742e0a591507da16971d
Josh Suereth <Joshua.Suereth@gmail.com>
2023-12-06 22:47:39 -0500

    Day 5 brute force solution

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -115,11 +115,27 @@ fn part_one_answer(input: &str) -> i64 {
       .unwrap()
 }
 
+// Super brute force, slow solution.
+fn part_two_answer(input: &str) -> i64 {
+    let (_, almanac) = 
+      parse_input(input)
+      .expect("Unable to parse input");
+    almanac.seeds.chunks(2)
+      .flat_map(|arr| {
+        let start = arr[0];
+        let length = arr[1];
+        start..(start+length)
+      })
+      .map(|seed| lookup_across(seed, &almanac.mappings))
+      .min()
+      .unwrap()
+}
+
 fn main() {
     let results = 
       fs::read_to_string("input.txt")
       .expect("Calibration file needs to exist.");
-    let result = part_one_answer(&results);
+    let result = part_two_answer(&results);
     println!("The smallest location is {result}")
 }
 
@@ -377,5 +393,41 @@ mod tests {
         humidity-to-location map:\n\
         60 56 37\n\
         56 93 4"), 35);
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part_two_answer("seeds: 79 14 55 13\n\
+        \n\
+        seed-to-soil map:\r\n\
+        50 98 2\r\n\
+        52 50 48\n\
+        \n\
+        soil-to-fertilizer map:\n\
+        0 15 37\n\
+        37 52 2\n\
+        39 0 15\n\
+        \n\
+        fertilizer-to-water map:\n\
+        49 53 8\n\
+        0 11 42\n\
+        42 0 7\n\
+        57 7 4\n\
+        \n\
+        water-to-light map:\n\
+        88 18 7\n\
+        18 25 70\n\
+        \n\
+        light-to-temperature map:\n\
+        45 77 23\n\
+        81 45 19\n\
+        68 64 13\n\
+        \n\
+        temperature-to-humidity map:\n\
+        0 69 1\n\
+        1 0 69\n\
+        \n\
+        humidity-to-location map:\n\
+        60 56 37\n\
+        56 93 4"), 46);
     }
 }
